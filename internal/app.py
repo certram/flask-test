2d03ebe9ae0302bf7a79ce88668be32b99c108d7
certram <liuzr2022@foxmail.com>
2025-08-20 11:13:04 +0800

    fix:toooaooa

--- internal/app.py
+++ internal/app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Dockerized Python App!"
+    
+    return "Hello from App!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5100)
